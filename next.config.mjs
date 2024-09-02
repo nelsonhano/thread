baebc8f31cb01673c6e2e4f625034dff03196479
koinyan nelson <koinyannelson0@gmail.com>
2024-09-02 22:58:30 +0100

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "**",
+            },
+        ],
+    },
+    experimental: {
+        serverActions: true,
+    },
+};
 
 export default nextConfig;
