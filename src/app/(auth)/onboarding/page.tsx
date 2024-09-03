56ffda379626e3367272960f010ab3af49f6e329
koinyan nelson <koinyannelson0@gmail.com>
2024-09-03 01:45:49 +0100

    clerk implemented

---
+++ src/app/(auth)/onboarding/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function OnBoarding() {
+  return (
+    <main>
+        <h1 className='head-text'>On Boarding</h1>
+    </main>
+  )
+}
---
+++ src/app/(root)/page.tsx
@@ -0,0 +1,13 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Threads</h1>
+
+      <div>
+        <UserButton afterSignOutUrl="/"/>
+      </div>
+    </main>
+  );
+}
